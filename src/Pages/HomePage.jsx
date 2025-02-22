253d784199bb07fc79b3df3b3ddfea6d2fb3c9f7
akshatgg <akshatg9636@gmail.com>
2025-02-22 06:07:53 +0000

    every setup is working properly

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className="container mx-auto">
             <h1>Welcome to the Home Page</h1>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,15 @@ import './index.css'
 import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-     <Router>
-    <App />
-     </Router>
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+   
+      <Router>
+        
+          <App />
+        
+      </Router>
+    
+  </>
+);
